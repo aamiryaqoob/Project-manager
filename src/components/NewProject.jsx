3ab14f72f495868c92e760c96c90704ae4e7759f
aamiryaqoob <aamiryaqoob965@gmail.com>
2025-08-30 20:24:27 +0500

    update

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -40,7 +40,7 @@ export default function NewProject({ onAdd, onCancel }) {
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
                     <li><button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button></li>
-                    <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:text-stone-950"
+                    <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:text-stone-600"
                         onClick={handleSave}>
                         Save
                     </button></li>
